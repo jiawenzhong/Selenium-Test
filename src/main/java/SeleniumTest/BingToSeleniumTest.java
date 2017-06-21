9694a58f01e13bfdc413aba108c657907675f9fd
zhongj1 <jia.wen.zhong@jhuapl.edu>
2017-06-21 16:45:46 -0400

    added selenium test project into java-testing

--- src/main/java/SeleniumTest/BingToSeleniumTest.java
+++ src/main/java/SeleniumTest/BingToSeleniumTest.java
@@ -12,44 +12,78 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class BingToSeleniumTest {
-  private WebDriver driver;
+  static WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
+  /**
+   * Setup headless mode to run without a browser
+   */
+  @BeforeClass
+  public static void setupHeadlessMode() {
+    String headlessProperty = System.getProperty("headless");
+    boolean isHeadlessTesting = Boolean.parseBoolean(headlessProperty);
+
+    // if the property was unspecified (such as running in an IDE), enable headless
+    if (null == headlessProperty) {
+      isHeadlessTesting = true;
+    }
+
+    // if headless is true, or unspecified, enable headless testing
+    if (isHeadlessTesting) {
+      driver = new HtmlUnitDriver();
+    } else {//else if headless is false, use google chrome
+      //need to set system property if using google chrome, and specify the location of the chromedriver.exe
+      System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+      driver = new ChromeDriver();
+    }
+  }
+
+  /**
+   * Set the base URL, and run the webpage
+   * @throws Exception
+   */
   @Before
   public void setUp() throws Exception {
-    System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
-    driver = new HtmlUnitDriver();
     baseUrl = "https://www.bing.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
   @Test
   public void testBingToSelenium() throws Exception {
-    driver.get(baseUrl + "/");
+    driver.get(baseUrl + "/");//open the base url, which is bing.com
     driver.findElement(By.id("sb_form_q")).clear();
-    driver.findElement(By.id("sb_form_q")).sendKeys("selenium");
-    driver.findElement(By.id("sb_form_go")).click();
+    driver.findElement(By.id("sb_form_q")).sendKeys("selenium");//type in the search contents
+    driver.findElement(By.id("sb_form_go")).click();//click search button
+
+    //check if the title is present in the webpage
     try {
       assertTrue(isElementPresent(By.cssSelector("h2..b_entityTitle")));
     } catch (Error e) {
       verificationErrors.append(e.toString());
     }
-    driver.findElement(By.linkText("Selenium - Web Browser Automation")).click();
+
+    driver.findElement(By.linkText("Selenium - Web Browser Automation")).click();//click on the link with this text
+
+    //check if the title is Brower Automation
     try {
       assertTrue(isElementPresent(By.linkText("Browser Automation")));
     } catch (Error e) {
       verificationErrors.append(e.toString());
     }
-    driver.findElement(By.linkText("Download")).click();
+
+    driver.findElement(By.linkText("Download")).click();//click on the download link
     // Warning: verifyTextPresent may require manual changes
+    //check if the new webpage has text Download
     try {
       assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Downloads[\\s\\S]*$"));
     } catch (Error e) {
       verificationErrors.append(e.toString());
     }
   }
+
+  //The code below are the code generated from Selenium IDE from FireFox
 
   @After
   public void tearDown() throws Exception {
--- src/main/java/SeleniumTest/SeleniumTest.java
+++ src/main/java/SeleniumTest/SeleniumTest.java
@@ -1,8 +1,10 @@
 package SeleniumTest;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,17 +12,41 @@ public class SeleniumTest  {
     //We should add @Test annotation that JUnit will run below method
 
     //Start to write our test method. It should ends with "Test"
-    public static void main(String [] args){
+    public static void main(String [] args) throws InterruptedException {
         //create firefox driver
         WebDriver driver;// = new FirefoxDriver();
-        System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
-        System.out.println(System.getProperty("webdriver.chrome.driver"));
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver", "C:\\firefoxdriver\\geckodriver.exe");
+//        System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+//        System.out.println(System.getProperty("webdriver.chrome.driver"));
+        driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("http://www.google.com");
-        driver.close();
-        driver.quit();
+        //open indeed
+        driver.get("https://www.indeed.com/");
+        //find the what field
+        Thread.sleep(2000);//slow down the process
+        driver.findElement(By.id("what")).sendKeys("Selenium");
+
+        //find the loaction field
+        Thread.sleep(2000);//slow down the process
+        driver.findElement(By.id("where")).clear();//clear the current text first
+        driver.findElement(By.id("where")).sendKeys("London");//then replace the new text
+
+        //click on find job buttons
+        Thread.sleep(2000);//slow down the process
+        driver.findElement(By.id("fj")).click();
+
+        //close the pop up
+        driver.findElement(By.id("prime-popover-close-button")).click();
+
+        //from job search result page, get page title and jobs count message
+        //search count
+        driver.getTitle();
+        driver.findElement(By.id("searchCount")).getText();
+        System.out.println("Title: " + driver.getTitle());
+        System.out.println("search count: " + driver.findElement(By.id("searchCount")).getText());
+        //driver.close();
+        //driver.quit();
 
 
     }
